b720147a8bef822ba43213fece11108f1fe5bbfc
dhamu42 <39159788+dhamu42@users.noreply.github.com>
2018-05-10 20:11:25 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
+import sys
+
 def write_file(file_name, data):
     ptr=open(file_name,'w')
     ptr.write(data)
     ptr.close()
     
     
-write_file('test.txt', "How are you.")
+write_file('test.txt', sys.argv[1])
